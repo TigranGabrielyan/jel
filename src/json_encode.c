c35ed970625ccd8bb2243e8cdfc2ebe6e6cefb34
Tigran Gabrielyan <gabrielyan_tigran@yahoo.com>
2022-07-18 22:03:33 +0400

    add initial support for yajl library

--- src/json_encode.c
+++ src/json_encode.c
@@ -5,11 +5,14 @@
 *
 */
 #include "json_encode.h"
+#include "yajl_parse.h"
 
 uint32_t
 Sum(
     uint16_t x,
     uint16_t y)
 {
+    yajl_handle hYajl = NULL;
+    yajl_free(hYajl);
     return x + y;
 }
