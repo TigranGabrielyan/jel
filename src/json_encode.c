50d064951261d779f645f7ca956e9e11144d2304
Tigran Gabrielyan <gabrielyan_tigran@yahoo.com>
2022-07-20 22:54:48 +0400

    APR basic integration for hashtable functionality

--- src/json_encode.c
+++ src/json_encode.c
@@ -7,6 +7,10 @@
 #include "json_encode.h"
 #include "yajl_parse.h"
 
+#include "apr_general.h"
+#include "apr_pools.h"
+#include "apr_hash.h"
+
 uint32_t
 Sum(
     uint16_t x,
@@ -14,5 +18,15 @@ Sum(
 {
     yajl_handle hYajl = NULL;
     yajl_free(hYajl);
+
+    apr_initialize();
+    apr_pool_t * pool;
+    apr_pool_create(&pool, NULL);
+    apr_hash_t * ht = apr_hash_make(pool);
+
+    apr_hash_clear(ht);
+    apr_pool_destroy(pool);
+    apr_terminate();
+
     return x + y;
 }
